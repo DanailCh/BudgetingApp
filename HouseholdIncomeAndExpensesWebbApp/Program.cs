efa797f11bf761e28bfde89c52ad95bef95a4010
DanailCh <114429701+DanailCh@users.noreply.github.com>
2024-03-28 21:03:26 +0200

    made radial options in view

--- HouseholdIncomeAndExpensesWebbApp/Program.cs
+++ HouseholdIncomeAndExpensesWebbApp/Program.cs
@@ -1,4 +1,8 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using HouseholdBudgetingApp.Data;
 var builder = WebApplication.CreateBuilder(args);
+
 
 builder.Services.AddApplicationDbContext(builder.Configuration);
 builder.Services.AddApplicationIdentity(builder.Configuration);
---
+++ HouseholdIncomeAndExpensesWebbApp/wwwroot/js/payBtn.js
@@ -0,0 +1,14 @@
+﻿
+   
+    const toggleButton = document.getElementById('PayButton');
+    const hiddenSection = document.getElementById('hiddenSection');
+
+   
+    toggleButton.addEventListener('click', function () {
+        
+        if (hiddenSection.style.display === 'none') {
+        hiddenSection.style.display = 'block';
+        } else {
+        hiddenSection.style.display = 'none';
+        }
+    });
